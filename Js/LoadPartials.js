f57e0f38ca4c48e735f7fec2ef3366677a20dac8
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2025-03-06 22:21:42 +0200

    Control Project

---
+++ Js/LoadPartials.js
@@ -0,0 +1,7 @@
+﻿const loadPartial = (path) => {
+    //уміє загружати дом
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", path, false); //загрузка буде проведена синхроно
+    xhr.send();
+    document.write(xhr.response); //записуємо результат роботи html-розмітку
+}
