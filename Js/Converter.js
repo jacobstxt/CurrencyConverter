00401ddb4a1f0ef609e1437c459e3bbc6286443b
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2025-03-08 15:43:29 +0200

    Add Axios

---
+++ Js/Converter.js
@@ -0,0 +1,41 @@
+﻿const Convert = (path) => {
+    axios.get(path)
+        .then(resp => {
+            const { data, status } = resp;
+            console.log("Resp status", status);
+
+            // Отримуємо введені дані
+            const amount = parseFloat(document.getElementById("amount").value);
+            const fromCurrency = document.getElementById("currency").value;
+            const toCurrency = document.getElementById("ToCurrency").value;
+            const resultField = document.getElementById("Total");
+
+            console.log("Вхідні дані:", { amount, fromCurrency, toCurrency });
+
+            if (isNaN(amount) || amount <= 0) {
+                resultField.innerText = "Некоректна сума";
+                return;
+            }
+
+            let fromRate = 1, toRate = 1;
+
+            
+            data.forEach(item => {
+                if (item.ccy === fromCurrency) fromRate = parseFloat(item.sale);
+                if (item.ccy === toCurrency) toRate = parseFloat(item.buy);
+            });
+
+            // Конвертація
+            let result;
+            if (fromCurrency === "UAH") {
+                result = amount / toRate;
+            } else if (toCurrency === "UAH") {
+                result = amount * fromRate;
+            } else {
+                result = (amount * fromRate) / toRate;
+            }
+
+            resultField.innerText = `≈ ${result.toFixed(2)} ${toCurrency}`;
+        })
+        .catch(error => console.error("Помилка завантаження курсу:", error));
+};
